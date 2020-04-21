6e8aa9b67e16aa04bde089aed6db2cf141a4f18e
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-04-20 21:19:28 -0300

    Criando o render da lista de negociações

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,11 +4,13 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoes');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update();
     }
 
     adiciona(evento: Event) {
