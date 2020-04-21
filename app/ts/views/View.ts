1c414a678c4bccec252f57c1e641c2c6bfbcabeb
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-04-21 14:50:36 -0300

    Melhorando view com tipos genéricos

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,9 +1,17 @@
-class View {
+class View<T> {
 
     protected _elemento: Element
 
     constructor(seletor: string) { 
         this._elemento = document.querySelector(seletor);
+    }
+
+    update(model: T): void {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string {
+        throw new Error('O método template deve ser implementado.');
     }
 
 }
